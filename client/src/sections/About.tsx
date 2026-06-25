035e5a656f7919d8a08aed091e9a0a5b14410707
Aditya Pandey <adityapandeycontact@gmail.com>
2026-06-25 14:49:03 +0530

    update in about.tsx

--- client/src/sections/About.tsx
+++ client/src/sections/About.tsx
@@ -89,7 +89,7 @@ function About() {
                             hover:-translate-y-1
                             hover:bg-white/10"
 						>
-							🚀 Looking for Fulltime / Internship /Freelance / Contract
+							🚀 Looking for Fulltime / Internship / Freelance / Contract
 						</div>
 
 						<div
