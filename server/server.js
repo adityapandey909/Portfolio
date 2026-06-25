202385c1bc20a427aca910268b12c0a5f3c6c9c1
Aditya Pandey <adityapandeycontact@gmail.com>
2026-06-25 15:04:06 +0530

    Use SMTP port 587

--- server/server.js
+++ server/server.js
@@ -16,11 +16,13 @@ app.use(
 app.use(express.json());
 
 const transporter = nodemailer.createTransport({
-	service: "gmail",
-	auth: {
-		user: process.env.EMAIL_USER,
-		pass: process.env.EMAIL_PASS,
-	},
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false,
+    auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+    },
 });
 
 app.get("/", (req, res) => {
